fd3b740cf1d096633a4397d3ebc2953ed046b91f
SensiPhi <lailtoneletrohits@hotmail.com>
2020-05-12 23:03:21 -0300

    Relógio finalizado

--- wesbos_30_days_js/relogio_de_parede/js/script.js
+++ wesbos_30_days_js/relogio_de_parede/js/script.js
@@ -18,3 +18,24 @@
   
   4.0 - Só resta rotacionar cada ponteiro com seus respectivos graus, isso pode ser feito com a propriedade style;
 */
+
+const ponteiroSegundos = document.querySelector(".relogio__ponteiro__segundo");
+const ponteiroMinutos = document.querySelector(".relogio__ponteiro__minuto");
+const ponteiroHoras = document.querySelector(".relogio__ponteiro__hora");
+
+function girarRelogio() {
+  const data = new Date();
+  const segundos = data.getSeconds();
+  const minutos = data.getMinutes();
+  const horas = data.getHours();
+
+  const segundosEmGraus = (segundos / 60) * 360 + 90;
+  const minutosEmGraus = (minutos / 60) * 360 + 90;
+  const horasEmGraus = (horas / 12) * 360 + 90;
+
+  ponteiroSegundos.style.transform = `translate(-97%, -50%) rotate(${segundosEmGraus}deg)`;
+  ponteiroMinutos.style.transform = `translate(-97%, -50%) rotate(${minutosEmGraus}deg)`;
+  ponteiroHoras.style.transform = `translate(-97%, -50%) rotate(${horasEmGraus}deg)`;
+}
+
+setInterval(girarRelogio, 1000);
